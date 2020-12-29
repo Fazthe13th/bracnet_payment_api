f970cc15fbfe0713565b05084cad5d0845d60db2
Fazthe13th <fazlulhaque.evan@protonmail.com>
2020-12-29 17:06:43 +0600

    send post request to habib vhai

--- bracnet_payment_api/sslcommerz_payment/views.py
+++ bracnet_payment_api/sslcommerz_payment/views.py
@@ -151,7 +151,7 @@ class SSLCommerzIPNView(GenericAPIView):
                        "store_amount": request.data['amount'],
                        "payment_method": 9}
             headers = {"Content-Type": "application/json"}
-            res = requests.post(url, data=payload, headers=headers)
+            requests.post(url, data=payload, headers=headers)
 
             return Response({'msg': 'Payment IPN received and Validated'}, status=status.HTTP_201_CREATED)
         except Exception:
