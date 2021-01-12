84e8fce563148976968999bd4b7bfc5c8d6af94c
Fazthe13th <fazlulhaque.evan@protonmail.com>
2021-01-12 16:05:00 +0600

    testing external database

--- bracnet_payment_api/bracnet_sc_bkash_api/settings.py
+++ bracnet_payment_api/bracnet_sc_bkash_api/settings.py
@@ -108,12 +108,19 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'HOST': os.environ.get('DB_HOST'),
+    #     'NAME': os.environ.get('DB_NAME'),
+    #     'USER': os.environ.get('DB_USER'),
+    #     'PASSWORD': os.environ.get('DB_PASS')
+    # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS')
+        'HOST': '202.168.232.8',
+        'NAME': 'bracnet_payment',
+        'USER': 'root',
+        'PASSWORD': 'Bracnet321#'
     }
 }
 
