ca09613b82e141884caf7abc282ed61553a3eaf5
Fazthe13th <fazlulhaque.evan@protonmail.com>
2021-01-14 18:13:34 +0600

    test certbot

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-until ./manage.py migrate
-do
-    echo "Waiting for db to be ready..."
-    sleep 2
-done
+# until ./manage.py migrate
+# do
+#     echo "Waiting for db to be ready..."
+#     sleep 2
+# done
 
 ./manage.py collectstatic --noinput
 
