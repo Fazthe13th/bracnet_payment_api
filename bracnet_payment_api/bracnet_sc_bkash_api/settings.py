cd21da70e75b988a54b1a5d1a77bd4732fae460e
Fazthe13th <fazlulhaque.evan@protonmail.com>
2021-01-17 13:38:27 +0600

    test ssl

--- bracnet_payment_api/bracnet_sc_bkash_api/settings.py
+++ bracnet_payment_api/bracnet_sc_bkash_api/settings.py
@@ -31,8 +31,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'zrqdsm$b+o*9k#@a(%gsxszr)okv--ur#=$!0==$p-5_ba)#k0'
 SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get('DEBUG', 0)))
-
+# DEBUG = bool(int(os.environ.get('DEBUG', 0)))
+DEBUG = True
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS')
 if ALLOWED_HOSTS_ENV:
