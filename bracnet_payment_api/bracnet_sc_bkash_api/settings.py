5702cb1b5787552484b61d278961bb634b2c9aaa
Fazthe13th <fazlulhaque.evan@protonmail.com>
2021-01-17 13:40:15 +0600

    test ssl

--- bracnet_payment_api/bracnet_sc_bkash_api/settings.py
+++ bracnet_payment_api/bracnet_sc_bkash_api/settings.py
@@ -33,10 +33,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 DEBUG = True
-ALLOWED_HOSTS = []
-ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS')
-if ALLOWED_HOSTS_ENV:
-    ALLOWED_HOSTS.extend(ALLOWED_HOSTS_ENV.split(','))
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS')
+# if ALLOWED_HOSTS_ENV:
+#     ALLOWED_HOSTS.extend(ALLOWED_HOSTS_ENV.split(','))
 
 AUTH_USER_MODEL = 'authentication.User'
 # Application definition
