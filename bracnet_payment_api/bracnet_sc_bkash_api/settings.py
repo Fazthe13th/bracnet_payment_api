5c972ecd76d9ef4b2e6647f437a38fe9ad1511a2
Fazthe13th <fazlulhaque.evan@protonmail.com>
2021-01-17 16:16:46 +0600

    test ssl

--- bracnet_payment_api/bracnet_sc_bkash_api/settings.py
+++ bracnet_payment_api/bracnet_sc_bkash_api/settings.py
@@ -180,11 +180,11 @@ CORS_ALLOW_ALL_ORIGINS = True
 # MEDIA_URL = '/media/'
 # STATIC_URL = '/django_static/'
 # STATIC_ROOT = BASE_DIR / 'django_static'
-STATIC_URL = '/static/static/'
-MEDIA_URL = '/static/media/'
+STATIC_URL = '/static'
+MEDIA_URL = '/static'
 
-STATIC_ROOT = '/vol/web/static'
-MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web'
+MEDIA_ROOT = '/vol/web'
 
 
 EMAIL_USE_TLS = True
