e10dc5eff90c16d034d1e4e3d58d8fff862adc53
Fazthe13th <fazlulhaque.evan@protonmail.com>
2021-01-17 18:08:35 +0600

    test ssl

--- bracnet_payment_api/bracnet_sc_bkash_api/urls.py
+++ bracnet_payment_api/bracnet_sc_bkash_api/urls.py
@@ -31,6 +31,7 @@ schema_view = get_schema_view(
         contact=openapi.Contact(email="contact@snippets.local"),
         license=openapi.License(name="BSD License"),
     ),
+    url='https://payment.bracnet.net/',
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
